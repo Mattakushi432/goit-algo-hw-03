5544ae3b0363811dcd90818b2ca917ae6d99db88
zakr1 <zakr1995@gmail.com>
2025-06-11 19:01:59 +0300

    TASK ONE!

--- Task_one.py
+++ Task_one.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 
 def get_days_from_today(date: str, date_format: str = "%Y-%m-%d") -> int:
+    """
+    Кількість повних тіжнів між заданою датою та поточною!
+
+
+    returns:
+        int: Кількість повних тижнів між датами!
+    """
     input_date = datetime.strptime(date, date_format).date()
     current_date = datetime.today().date()
     delta_days = abs((current_date - input_date).days)
