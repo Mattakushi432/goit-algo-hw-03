390d5b78421ae1bfdb4dc371581d40ed3c426fa4
zakr1 <zakr1995@gmail.com>
2025-06-12 10:58:47 +0300

    change code Task_one.py

--- Task_one.py
+++ Task_one.py
@@ -1,18 +1,51 @@
 from datetime import datetime
 
 
-def get_days_from_today(date: str, date_format: str = "%Y-%m-%d") -> int:
-    """
-    Кількість повних тіжнів між заданою датою та поточною!
+def get_days_from_today(date: str) -> int:
+    try:
+        format_date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print(f"Помилка: Неправильний формат дати '{date}'. Очікується 'PPPP-MM-ДД'")
+        return None
+
+    # Отримання поточної дати без врахування часу!
+    current_date = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+    time_difference = current_date - format_date
+    return time_difference.days
 
 
-    returns:
-        int: Кількість повних тижнів між датами!
-    """
-    input_date = datetime.strptime(date, date_format).date()
-    current_date = datetime.today().date()
-    delta_days = abs((current_date - input_date).days)
-    return delta_days
+if __name__ == "__main__":
+    """Перевірка з поточною датою"""
+    print(f"Поточна дата: {datetime.today().strftime('%Y-%m-%d')}")
 
+    date1 = "2021-10-09"
+    days1 = get_days_from_today(date1)
+    if days1 is not None:
+        print(f"Кількість днів від {date1} до сьогодны: {days1}")
 
-print(get_days_from_today("2026-06-11"))
+    date2 = "2025-06-15"
+    days2 = get_days_from_today(date2)
+    if days2 is not None:
+        print(f"Кількість днів від {date2} до сьогодны: {days2}")
+
+    date3 = "2025-06-10"
+    days3 = get_days_from_today(date3)
+    if days3 is not None:
+        print(f"Кількість днів від {date3} до сьогодны: {days3}")
+
+    date4 = "2025-06-12"
+    days4 = get_days_from_today(date4)
+    if days4 is not None:
+        print(f"Кількість днів від {date4} до сьогодны: {days4}")
+
+    date5 = "2024-01-01"
+    days5 = get_days_from_today(date5)
+    if days5 is not None:
+        print(f"Кількість днів від {date5} до сьогодны: {days5}")
+
+"""Перевірка та обробка винятків!"""
+date_invalid = "2023/12/31"
+get_days_from_today(date_invalid)
+
+date_another_invalid = "abc-def-ghi"
+get_days_from_today(date_another_invalid)
