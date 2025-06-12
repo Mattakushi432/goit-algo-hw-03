08921334c920e4949e2eb11b9e1ebabcaf9fa230
zakr1 <zakr1995@gmail.com>
2025-06-12 11:01:18 +0300

    change code

--- Task_three.py
+++ Task_three.py
@@ -1,22 +1,16 @@
 import re
 
 
-def normalize_phone(phone_number):
-    for phone in phone_number:
-        cleaned_number = re.sub(r'[^0-9]', '', phone)
-        if not cleaned_number.startswith('+'):
-            if cleaned_number.startswith('380'):
-                cleaned_number = '+' + cleaned_number
-            else:
-                cleaned_number = '+38' + cleaned_number
-        print(cleaned_number)
+def normalize_phone(phone_number: str) -> str:
+    sanitized_number = re.sub(r'[^\d+]', '', phone_number)
+    if sanitized_number.startswith('380') and not sanitized_number.startswith('+'):
+        sanitized_number = '380' + sanitized_number
 
+    return sanitized_number
 
-phone_numbers = [
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11 asf  "
-]
-normalize_phone(phone_numbers)
+raw_numbers = [
+        "067\t123 4567",
+    ]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
