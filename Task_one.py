41224f360eaf71577a8db09ddedc2a0d5d1d9a18
zakr1 <zakr1995@gmail.com>
2025-06-13 19:04:45 +0300

    change code

--- Task_one.py
+++ Task_one.py
@@ -21,27 +21,27 @@ if __name__ == "__main__":
     date1 = "2021-10-09"
     days1 = get_days_from_today(date1)
     if days1 is not None:
-        print(f"Кількість днів від {date1} до сьогодны: {days1}")
+        print(f"Кількість днів від {date1} до сьогодні: {days1}")
 
     date2 = "2025-06-15"
     days2 = get_days_from_today(date2)
     if days2 is not None:
-        print(f"Кількість днів від {date2} до сьогодны: {days2}")
+        print(f"Кількість днів від {date2} до сьогодні: {days2}")
 
     date3 = "2025-06-10"
     days3 = get_days_from_today(date3)
     if days3 is not None:
-        print(f"Кількість днів від {date3} до сьогодны: {days3}")
+        print(f"Кількість днів від {date3} до сьогодні: {days3}")
 
     date4 = "2025-06-12"
     days4 = get_days_from_today(date4)
     if days4 is not None:
-        print(f"Кількість днів від {date4} до сьогодны: {days4}")
+        print(f"Кількість днів від {date4} до сьогодні: {days4}")
 
     date5 = "2024-01-01"
     days5 = get_days_from_today(date5)
     if days5 is not None:
-        print(f"Кількість днів від {date5} до сьогодны: {days5}")
+        print(f"Кількість днів від {date5} до сьогодні: {days5}")
 
 """Перевірка та обробка винятків!"""
 date_invalid = "2023/12/31"
