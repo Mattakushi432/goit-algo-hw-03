95e01c14270848de8263778490db2c5ed9b9329c
zakr1 <zakr1995@gmail.com>
2025-06-13 15:56:06 +0300

    change code Task_two.py and Task_three.py

--- Task_two.py
+++ Task_two.py
@@ -1,7 +1,7 @@
 import random
 
 
-def get_numbers_ticket(max: int, min: int, quantity: int) -> list[int]:
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
     if not (1 <= min <= max <= 1000 and 0 < quantity <= (max - min + 1)):
         return []
 
@@ -12,5 +12,8 @@ def get_numbers_ticket(max: int, min: int, quantity: int) -> list[int]:
     return sorted(list(numbers))
 
 
-lottery_ticket = get_numbers_ticket(50, 10, 5)
-print(f"Ваші лотерейні номери (50, 10, 5 чисел): {lottery_ticket}")
+lottery_ticket = get_numbers_ticket(10, 50, 5)
+print(f"Ваші лотерейні номери (10, 50, 5 чисел): {lottery_ticket}")
+
+lottery_ticket_test = get_numbers_ticket(1, 49, 6)
+print(f"Ваші лотерейні номери (1, 49, 6 чисел): {lottery_ticket_test}")
